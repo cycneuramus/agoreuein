20c112963b6b5c1f4b64eb1a36e117996e5ae957
a <a@local>
2022-03-24 20:36:48 +0100

    don't be stupid

--- script/agoreuein.py
+++ script/agoreuein.py
@@ -95,8 +95,8 @@ async def main():
             if new_contact is not None:
                 await send_msg(client, new_contact.users[0])
 
-            minutes = randint(1800, 3600) * 60
-            logging.info(f"Sleeping for {minutes} seconds")
+            minutes = randint(1800, 3600) / 60
+            logging.info(f"Sleeping for {minutes} minutes")
             await asyncio.sleep(minutes)
 
 
